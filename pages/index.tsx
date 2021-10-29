48e69da097160a154dd4d088a8792066bddb115f
Lukas <69743171+quick007@users.noreply.github.com>
2021-10-29 09:39:33 -0700

    temp force upper lunch

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,7 +149,7 @@ export default function Home() {
   if (!cookies.lunch) {
     setCookie("lunch", true);
   }
-  const [lowerLunch, setLowerLunch] = useState(true);
+  const [lowerLunch, setLowerLunch] = useState(false);
   return (
     <>
       <Head>
@@ -175,7 +175,7 @@ export default function Home() {
             </div> : "" }
             <br />
             <div className="inline-flex rounded-md bg-gradient-to-br from-green-200 to-green-300 dark:from-green-400 dark:to-green-500 backdrop-filter backdrop-blur p-1 mt-6 space-x-2 select-none">
-              <div className={"rounded-md p-1 font-medium hover:ring-2 transition cursor-pointer " + (lowerLunch ? "bg-gray-300 bg-opacity-50" : "")} onClick={() => setLowerLunch(true)}>Lower Lunch</div>
+              <div className={"rounded-md p-1 font-medium hover:ring-2 transition cursor-pointer " + (lowerLunch ? "bg-gray-300 bg-opacity-50" : "")} onClick={() => setLowerLunch(false)}>Lower Lunch</div>
               <div className={"rounded-md p-1 font-medium hover:ring-2 transition cursor-pointer " + (!lowerLunch ? "bg-gray-300 bg-opacity-50" : "")} onClick={() => setLowerLunch(false)}>Upper Lunch</div>
             </div>
             {daily(lowerLunch)}
